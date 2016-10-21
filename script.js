60313effb172c5f5a7f2247177467d7cdd4791c1
ZacSmir <Zach@smirin.us>
2016-10-21 08:43:03 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 onNewListItem("product-list", function(product) {
   console.log(product.picture)
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
-  var image = $("<img class='product-image'>").attr("src", "https://cbswingbowl2.files.wordpress.com/2012/01/gentleman-jerry-1.jpg?w=709")  
+  var image = $("<img class='product-image'>").attr("src", product.picture)  
 
   $(".product-list-container").append(nameHeader, image)
 })
